28b326ca1195c43b7831df742f96448f9fd36985
marcelorang <mclowebdev@gmail.com>
2023-08-02 16:47:31 -0300

    feat: adding Stripe payment

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,6 +2,7 @@
 
 import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
@@ -14,6 +15,7 @@ const NavbarActions = () => {
       setIsMounted(true);  
     }, []);
 
+    const router = useRouter();
     const cart = useCart();
 
 
@@ -24,7 +26,7 @@ const NavbarActions = () => {
 
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex items-center rounded-full bg-black px-4 py-2">
+            <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
                 <ShoppingBag
                   size={20}
                   color="white"  
